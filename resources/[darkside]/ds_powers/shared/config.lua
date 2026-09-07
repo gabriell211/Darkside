2410ae4e1e1598b0f8f9bebe8dda34f5016a8ea7
Gabriel Barcelos <102088315+gabriell211@users.noreply.github.com>
2026-09-07 20:51:58 -0300

    feat: define starter powers

---
+++ resources/[darkside]/ds_powers/shared/config.lua
@@ -0,0 +1,21 @@
+DarkSidePowers = {
+    shadow_step = {
+        id = 'shadow_step',
+        label = 'Passo Sombrio',
+        clan = 'darkside',
+        category = 'mobility',
+        energyCost = 25,
+        cooldownMs = 12000,
+        range = 8.0
+    },
+
+    fallen_sense = {
+        id = 'fallen_sense',
+        label = 'Sentido Caído',
+        clan = 'fallen_angels',
+        category = 'sensory',
+        energyCost = 20,
+        cooldownMs = 15000,
+        durationMs = 5000
+    }
+}
