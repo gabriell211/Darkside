b65b66a200a9f1efb97393988d16c187bb74a768
Gabriel Barcelos <102088315+gabriell211@users.noreply.github.com>
2026-09-07 20:51:15 -0300

    feat: add ds_clans manifest

---
+++ resources/[darkside]/ds_clans/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+lua54 'yes'
+
+author 'DarkSide'
+description 'DarkSide clan system'
+version '0.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    'rsg-core',
+    'ds_core'
+}
