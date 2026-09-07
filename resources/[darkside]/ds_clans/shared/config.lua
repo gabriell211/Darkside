23288774df0fe0bd037317792c1c629e546bef27
Gabriel Barcelos <102088315+gabriell211@users.noreply.github.com>
2026-09-07 20:51:24 -0300

    feat: define initial playable clans

---
+++ resources/[darkside]/ds_clans/shared/config.lua
@@ -0,0 +1,28 @@
+DarkSideClans = {
+    darkside = {
+        id = 'darkside',
+        label = 'DarkSide',
+        concept = 'Uma Cidade, Dois Mundos',
+        description = 'Clã sobrenatural jogável. Sua origem definitiva ainda será fechada pela lore.',
+        ranks = {
+            [0] = 'Iniciado',
+            [1] = 'Ascendente',
+            [2] = 'Guardião',
+            [3] = 'Regente'
+        },
+        starterPower = 'shadow_step'
+    },
+
+    fallen_angels = {
+        id = 'fallen_angels',
+        label = 'Anjos Caídos',
+        description = 'Seres celestiais exilados ou separados de sua antiga ordem. Não são demônios.',
+        ranks = {
+            [0] = 'Exilado',
+            [1] = 'Portador',
+            [2] = 'Arauto',
+            [3] = 'Serafim Caído'
+        },
+        starterPower = 'fallen_sense'
+    }
+}
