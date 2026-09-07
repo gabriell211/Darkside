5567fb95f86934c2dbc1c7ed7b6b86ab3e8e4fe8
Gabriel Barcelos <102088315+gabriell211@users.noreply.github.com>
2026-09-07 20:50:30 -0300

    feat: add ds_core config

---
+++ resources/[darkside]/ds_core/shared/config.lua
@@ -0,0 +1,10 @@
+DarkSideConfig = DarkSideConfig or {}
+
+DarkSideConfig.Debug = true
+DarkSideConfig.DefaultClan = nil
+DarkSideConfig.StartingEnergy = 100
+DarkSideConfig.MaxEnergy = 100
+DarkSideConfig.EnergyRegenPerTick = 2
+DarkSideConfig.EnergyRegenTickMs = 2000
+
+DarkSideConfig.Locale = 'pt-BR'
