7d9482e568e920cd6c20fa6323199a3553b1028e
Gabriel Barcelos <102088315+gabriell211@users.noreply.github.com>
2026-09-07 21:05:33 -0300

    feat(ai): add default AI profile

---
+++ resources/[darkside]/ds_ai/shared/config.lua
@@ -0,0 +1,10 @@
+DarkSideAIConfig = {
+    ThinkInterval = 500,
+    DefaultProfile = {
+        detectionRange = 35.0,
+        loseRange = 55.0,
+        attackRange = 1.8,
+        searchDuration = 8000,
+        moveSpeed = 2.0
+    }
+}
