10b5f05b8fe5cccce96585d9095977d93411705b
Gabriel Barcelos <102088315+gabriell211@users.noreply.github.com>
2026-09-07 21:06:16 -0300

    feat(killers): add killer resource manifest

---
+++ resources/[darkside]/ds_killers/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+lua54 'yes'
+
+author 'DarkSide'
+description 'DarkSide killer NPC system'
+version '0.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    'rsg-core',
+    'ds_core',
+    'ds_ai'
+}
