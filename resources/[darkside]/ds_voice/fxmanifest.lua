8767a1a1777581b0e0980fdfd5c250dcc0d57112
Gabriel Barcelos <102088315+gabriell211@users.noreply.github.com>
2026-09-07 22:12:54 -0300

    feat: add DarkSide pma-voice adapter

---
+++ resources/[darkside]/ds_voice/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+lua54 'yes'
+
+author 'DarkSide'
+description 'DarkSide voice adapter for pma-voice on RedM'
+version '0.1.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+dependencies {
+    'pma-voice'
+}
